589b89f34e59e491736b8520076deffdca7155e0
Henrique <henriqueurbano21@gmail.com>
2018-09-24 16:17:16 -0300

    Inclusão de código inicial de Json

---
+++ notes-node/playground/json.js
@@ -0,0 +1,25 @@
+//var obj = {
+    //name: 'Henrique'
+//};
+//var stringObj = JSON.stringify(obj);
+//console.log(typeof stringObj);
+//console.log(stringObj);
+
+//var personString = '{"name":"Henrique", "age" : 23}';
+//var person = JSON.parse(personString);
+//console.log(typeof person);
+//console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+    title: 'Some Title',
+    body: 'Some Body'
+};
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title)
