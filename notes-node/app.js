b7556c54e9afcff636dbbe68742542f45c3a3a31
Henrique <henriqueurbano21@gmail.com>
2018-09-28 15:02:41 -0300

    Added the Add Note and List Functionalities

--- notes-node/app.js
+++ notes-node/app.js
@@ -8,20 +8,23 @@ const yargs = require('yargs');
 
 const argv = yargs.argv;
 
-var command = process.argv[2];
-console.log('Command: ', command);
-console.log('Process', process.argv);
-console.log('Yargs', argv);
+var command = argv._[0];
 
 if (command === 'add'){
-	console.log('Adding new note...');
+	notes.addNote(argv.title, argv.body);
 }
  else if (command === 'list'){
-	console.log('Listing all notes...');
+	notes.getAll();
+}
+ else if (command === 'read'){
+	notes.getNote(argv.title);
+}
+ else if(command ==='remove'){
+	notes.removeNote(argv.title);
 }
  else{
-	console.log('Command not recognized.');
-};
+	console.log('Command not recognized');
+ };
 //var res = notes.addNote();
 //var calc = notes.add(7, 6);
 //console.log(res);
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,11 +1,53 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = function(){
-	console.log('addNote');
-	return 'New Note';
+const fs = require('fs');
+
+var addNote = (title, body) => {
+	var notes = [];
+	try{
+	var notesstring = fs.readFileSync('notes-data.json');
+	notes = JSON.parse(notesstring);
+	}
+	catch(e){
+
+	}	
+	var note = {
+		title,
+		body
+	};
+
+	var duplicateNotes = notes.filter((note) => note.title === title);
+
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	} else{
+		console.log('This title has already been used!');
+	}	
 };
 
-module.exports.add = function(a,x){
-	var result = a + x;
+var getAll = () => {
+	var result = '';
+	try{
+	var jsonfile = fs.readFileSync('notes-data.json');
+	result = JSON.parse(jsonfile);
+	} catch(e){
+		result = 'No notes to be fetched!'
+	}
 	console.log(result);
+};
+
+var getNote = (title) => {
+	console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+	console.log('Removing note', title);
+};
+
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	removeNote
 }
